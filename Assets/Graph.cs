89b5c4e52cae465606a897647f7c28be88c4b20c
Unknown <almabekov@gmail.com>
2018-05-19 23:42:08 -0300

    part 2 of the graph tutorial done

--- Assets/Graph.cs
+++ Assets/Graph.cs
@@ -4,4 +4,24 @@ public class Graph : MonoBehaviour
 {
 
     public Transform pointPrefab;
+    [Range(10,100)]
+    public int resolution = 10;
+
+    void Awake()
+    {
+        float step = 2f / resolution;
+        Vector3 scale = Vector3.one*step;
+        Vector3 position;
+        
+        for (int i=0;i<resolution;i++) {
+            Transform point = Instantiate(pointPrefab);
+            position.x = (i+0.5f)*step-1f;
+            position.y = position.x*position.x;
+            position.z = 0f;
+            point.localPosition = position;
+            point.localScale = scale;
+            point.SetParent(transform, false);
+        }
+        
+    }
 }
